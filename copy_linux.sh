540f449b1f954c4680be4029fa39b078dca15114
Wang Renxin <hellotony521@qq.com>
2020-12-14 18:14:29 +0800

    *updated script.

---
+++ copy_linux.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+
+ARCH=$(arch)
+BASEDIR=$(dirname $0)
+SUBDIR=""
+if [ "$ARCH" = "x86_64" ]; then
+  SUBDIR="x64"
+else
+  SUBDIR="x86"
+fi
+
+if [ -d "$BASEDIR/build/.libs" ]; then
+  cp -f "$BASEDIR/include/SDL_config.h" "$BASEDIR/lib/$SUBDIR/linux"
+  cp -f "$BASEDIR/build/.libs/libSDL2.a" "$BASEDIR/lib/$SUBDIR/"
+  cp -f "$BASEDIR/build/.libs/libSDL2.la" "$BASEDIR/lib/$SUBDIR/"
+  cp -f "$BASEDIR/build/.libs/libSDL2-2.0.so.0.12.0" "$BASEDIR/lib/$SUBDIR/"
+  cp -f "$BASEDIR/build/.libs/libSDL2-2.0.so.0" "$BASEDIR/lib/$SUBDIR/"
+  cp -f "$BASEDIR/build/.libs/libSDL2.so" "$BASEDIR/lib/$SUBDIR/"
+fi
+
+echo Ok.
