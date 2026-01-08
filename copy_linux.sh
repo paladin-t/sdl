151d6fdd7b2901c8992921bb05ec36ad290e72ea
tony <hellotony521@qq.com>
2026-01-08 16:32:14 +0800

    *Updated the copy script for linux.

--- copy_linux.sh
+++ copy_linux.sh
@@ -19,7 +19,7 @@ if [ -d "$BASEDIR/build/.libs" ]; then
   cp -f "$BASEDIR/include/SDL_config.h" "$BASEDIR/lib/$SUBDIR/linux/"
   cp -f "$BASEDIR/build/.libs/libSDL2.a" "$BASEDIR/lib/$SUBDIR/"
   cp -f "$BASEDIR/build/.libs/libSDL2.la" "$BASEDIR/lib/$SUBDIR/"
-  cp -f "$BASEDIR/build/.libs/libSDL2-2.0.so.0.12.0" "$BASEDIR/lib/$SUBDIR/"
+  cp -f "$BASEDIR/build/.libs/libSDL2-2.0.so.0.3200.10" "$BASEDIR/lib/$SUBDIR/"
   cp -f "$BASEDIR/build/.libs/libSDL2-2.0.so.0" "$BASEDIR/lib/$SUBDIR/"
   cp -f "$BASEDIR/build/.libs/libSDL2.so" "$BASEDIR/lib/$SUBDIR/"
 fi
